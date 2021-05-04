d5b5db071e24e2decbbb6fb67e6ce29152121c62
feiw <viffey1991@gmail.com>
2021-05-04 11:23:36 +1000

    Enter

---
+++ README.md
@@ -0,0 +1,5 @@
+1. routes
+2. Login/Logout switch
+3. Chartjs. Line charts and Bar charts only. 
+   Some use of charting or other information graphics to show how the scores vary over time. Some client side filtering and support for partial match search is expected.
+4. Error handling
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -13,6 +13,9 @@ import SearchBar from "../components/SearchBar";
 export default function Search(){ 
   const [search, setSearch] = useState("Finland");
   const {loading, rowData, error} = useAllData(search);
+
+  let chartData = rowData.map((e) => e.score);
+  console.log(chartData);
   
   const columns = [
     {headerName: "Year", field: "year"},
@@ -21,57 +24,19 @@ export default function Search(){
   ];
 
   //============= line chart ===============================
+
   const data = {
     labels: [2015,2016,2017,2018,2019,2020],
     datasets: [
       {
         label: 'The change of rank from 2015 to 2020',
         fill: false,
-        lineTension: 0.1,
         backgroundColor: 'rgba(75,192,192,0.4)',
         borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [6,5,5,1,1,1]
+        //data:{chartData}
+        data: ["7.2", "6.3", "7.8", "6.4", "7,3", "7.6"]
       }
     ]
-  };
-  
-  const lineOptions = {
-    scales: {
-      xAxes: [{
-        gridLines: {
-          display: false,
-        },
-      }],
-      yAxes: [{
-        // stacked: true,
-        gridLines: {
-          display: false,
-        },
-        ticks: {
-          reverse: true,
-          },
-        
-      }],
-    },
-    legend: {
-      display: false,
-    },
-    tooltips: {
-      enabled: false,
-    },
   };
   
 
@@ -102,7 +67,7 @@ export default function Search(){
       </div>
       <br />
       <div className="lineChart">
-        <Line data={data} options={lineOptions} />
+        <Line data={data} />
       </div>
     </div>
     
